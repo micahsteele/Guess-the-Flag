72d4ee53a1a94e640cb18b36c8047e32bf61570c
micahsteele <micahs66@gmail.com>
2022-11-29 22:05:39 -0500

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Micah Steele on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
